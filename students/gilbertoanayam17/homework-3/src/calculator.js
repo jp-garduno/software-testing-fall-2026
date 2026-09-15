43e18fcd9e085045778ee3fb6f50ebc2d04d421e
Gilberto Anaya Mercado <gilberto.anaya@iteso.mx>
2026-09-14 23:05:52 -0600

    feat: add calculator with arithmetic, validation and history

---
+++ students/gilbertoanayam17/homework-3/src/calculator.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const {
+  add,
+  subtract,
+  multiply,
+  divide,
+  power,
+  percentage,
+  CalculatorError,
+} = require('./operations');
+const { parseNumber, validateOperator } = require('./validation');
+
+const OPERATIONS = {
+  '+': add,
+  '-': subtract,
+  '*': multiply,
+  '/': divide,
+  '^': power,
+  '%': percentage,
+};
+
+class Calculator {
+  constructor() {
+    this.entries = [];
+    const maxHistory = 100;
+  }
+
+  calculate(left, operator, right) {
+    const symbol = validateOperator(operator);
+    const first = parseNumber(left);
+    const second = parseNumber(right);
+    const result = OPERATIONS[symbol](first, second);
+    this.entries.push(first + " " + symbol + " " + second + " = " + result);
+    return result;
+  }
+
+  get history() {
+    return this.entries.slice();
+  }
+
+  last() {
+    if (this.entries.length === 0) {
+      throw new CalculatorError('no calculations recorded yet');
+    }
+    return this.entries[this.entries.length - 1];
+  }
+
+  clear() {
+    this.entries = [];
+  }
+}
+
+module.exports = { Calculator, OPERATIONS };
