efa85bb2347643e1836d40379ebe6cf180d23142
Larquitoskool <carlosariel.rubioespinosa@gmail.com>
2026-09-22 21:05:45 -0600

    fix: resolve CI import path and add requirements

--- students/larquitoskool/homework-4/tests/conftest.py
+++ students/larquitoskool/homework-4/tests/conftest.py
@@ -1,6 +1,10 @@
 import pytest
-from src.banking_system import BankAccount
+import sys
+import os
 
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from src.banking_system import BankAccount
 
 @pytest.fixture
 def savings_account():
